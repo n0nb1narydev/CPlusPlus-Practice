7c15fa2668e75b949b0eaa43910450fa8aa86a4a
Mel Rumsey <melsrumsey@gmail.com>
2020-10-20 11:09:09 -0500

    notes for game written

---
+++ TripleXGame.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+
+// main or game function
+
+// You are an explorer who has found a strange looking cave. Upon entering you see a strange room filled with chests of varying sizes. You notice, all of them have a locking mechanism that has 3 number dials. Start with the smallest chest first to crack the code an gain the loot. But, hurry. There is probably someone or some...thing supposed to be guarding this room.
+
+// Need to find a valid combination of codes
+
+// starts easy and becomes much harder
+
+
+// Main function
+
+// int main()
+// {
+//     int difficulty = 2;
+//     int maxDifficulty = 10;
+//     while (difficulty <= maxDifficulty)
+//     {
+//         PlayGame(difficulty);
+//         std::cin.clear(); // clears the failbit
+//         std::cin.ignore(); // discards the buffer
+//         ++difficulty;
+//     }
+//      std::cout << "You have unlocked all of the chests! Uh oh... You see a large pair of eyes peering at you from the darkness. RUN!"
+//      return 0; // exits the program with no errors
+// }
