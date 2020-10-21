14a1d3d3c1708cb104e80fb87d9639c2a3a1d8dc
Mel Rumsey <melsrumsey@gmail.com>
2020-10-21 14:28:09 -0500

    added welcome

--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,10 +3,14 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();
+    Super::BeginPlay();                              // Game won't work without this
+    PrintLine("Mooooo! Let's play some Bull Cows!"); // Just like std::cout << "Welcome!";
+    PrintLine("Press enter to start...");            // For strings, instead of std::string Word = "Welcome!"; use FString.
+                                                     // instead of int, use int32
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString & Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = "dung";
 }
