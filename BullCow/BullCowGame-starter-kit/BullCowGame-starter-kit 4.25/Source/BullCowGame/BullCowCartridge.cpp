263041b0a8ce104b3a3ced252f60f973595089dd
Mel Rumsey <melsrumsey@gmail.com>
2020-10-21 14:30:39 -0500

    added TEXT macro

--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
@@ -3,14 +3,14 @@
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
-    Super::BeginPlay();                              // Game won't work without this
-    PrintLine("Mooooo! Let's play some Bull Cows!"); // Just like std::cout << "Welcome!";
-    PrintLine("Press enter to start...");            // For strings, instead of std::string Word = "Welcome!"; use FString.
-                                                     // instead of int, use int32
+    Super::BeginPlay();                                    // Game won't work without this
+    PrintLine(TEXT("Mooooo! Let's play some Bull Cows!")); // Just like std::cout << "Welcome!";
+    PrintLine(TEXT("Press enter to start..."));            // For strings, instead of std::string Word = "Welcome!"; use FString.
+                                                           // instead of int, use int32
 }
 
-void UBullCowCartridge::OnInput(const FString & Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "dung";
+    FString HiddenWord = TEXT("dung"); // using TEXT macro for strings
 }
