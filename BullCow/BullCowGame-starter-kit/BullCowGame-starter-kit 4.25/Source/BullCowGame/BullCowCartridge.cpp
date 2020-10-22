e97b9b71e40c48553aa806b73613972e6f4036a9
Mel Rumsey <melsrumsey@gmail.com>
2020-10-22 10:38:11 -0500

    save

--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,13 +4,43 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();                                    // Game won't work without this
-    PrintLine(TEXT("Mooooo! Let's play some Bull Cows!")); // Just like std::cout << "Welcome!";
-    PrintLine(TEXT("Press enter to start..."));            // For strings, instead of std::string Word = "Welcome!"; use FString.
-                                                           // instead of int, use int32
+    PrintLine(TEXT("Mooooo! Let's play some Bull Cows!")); // Just like std::cout << "Welcome!";                   // using TEXT macro for strings
+    PrintLine(TEXT("Guess the 4 letter word!"));           // make length variable in place of 4          // For strings, instead of std::string Word = "Welcome!"; use FString.
+    HiddenWord = TEXT("dung");                             // instead of int, use int32
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("dung"); // using TEXT macro for strings
+    int lives = 5;
+
+    while (lives > 0)
+    {
+        if (Input == HiddenWord)
+        {
+            PrintLine("Huzzah! You guessed the hidden word!");
+            break;
+        }
+        else
+        {
+            PrintLine("Moo... You got it wrong. Try again!");
+            lives--;
+            continue;
+        }
+    }
+    if (lives == 0)
+    {
+        PrintLine("The hidden word was: " + HiddenWord + "...");
+        PrintLine("You ran out of lives. :( Play again?");
+    }
 }
+
+/* 
+Game Loop 
+Start the game
+Set the Hidden Word
+Prompt for the gues
+is the guess correct
+did they win or lose?
+play again?
+*/
--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.h
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.h
@@ -6,16 +6,15 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 {
 	GENERATED_BODY()
 
-	public:
+public:
 	virtual void BeginPlay() override;
-	virtual void OnInput(const FString& Input) override;
+	virtual void OnInput(const FString &Input) override;
 
-	// Your declarations go below!
-	private:
-	
+private:
+	FString HiddenWord;
 };
