cc564c6a3bb4e7c1153be16e581d87d845145499
Mel Rumsey <melsrumsey@gmail.com>
2020-10-22 11:19:26 -0500

    psuedocode

--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,17 +5,21 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();                                    // Game won't work without this
     PrintLine(TEXT("Mooooo! Let's play some Bull Cows!")); // Just like std::cout << "Welcome!";                   // using TEXT macro for strings
-    PrintLine(TEXT("Guess the 4 letter word!"));           // make length variable in place of 4          // For strings, instead of std::string Word = "Welcome!"; use FString.
-    HiddenWord = TEXT("dung");                             // instead of int, use int32
+    // For strings, instead of std::string Word = "Welcome!"; use FString.
+
+    // Setting up game
+    HiddenWord = TEXT("dung"); // Set the HiddenWord
+    lives = 4;                 // instead of int, use int32
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    int lives = 5;
 
     while (lives > 0)
     {
+        PrintLine(TEXT("Guess the 4 letter word!"));
+
         if (Input == HiddenWord)
         {
             PrintLine("Huzzah! You guessed the hidden word!");
@@ -24,15 +28,19 @@ void UBullCowCartridge::OnInput(const FString &Input) // When the player hits en
         else
         {
             PrintLine("Moo... You got it wrong. Try again!");
-            lives--;
-            continue;
         }
+        // check if isogram
+        // check if correct number of chars
+        lives--;
     }
+
     if (lives == 0)
     {
         PrintLine("The hidden word was: " + HiddenWord + "...");
-        PrintLine("You ran out of lives. :( Play again?");
+        PrintLine("You ran out of lives. :(");
     }
+    // Prompt to play again
+    PrintLine("Would you like to play again?")
 }
 
 /* 
--- BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.h
+++ BullCow/BullCowGame-starter-kit/BullCowGame-starter-kit 4.25/Source/BullCowGame/BullCowCartridge.h
@@ -17,4 +17,5 @@ public:
 
 private:
 	FString HiddenWord;
+	int lives;
 };
